50dec489b43f44a99e67d1de118e41da4b2fe4c3
jiang <19939600096@163.com>
2021-01-15 17:26:24 +0800

    添加knife4j

--- weidun-admin/src/main/java/org/jiang/admin/config/SwaggerConfig.java
+++ weidun-admin/src/main/java/org/jiang/admin/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package org.jiang.admin.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.jiang.common.config.BaseSwaggerConfig;
 import org.jiang.common.domain.SwaggerProperties;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
 public class SwaggerConfig extends BaseSwaggerConfig {
     @Override
     public SwaggerProperties swaggerProperties() {
